3d268abffd993edf3a959fd6d743c8fd35539071
VogelDev <rvogel@mc3.edu>
2015-02-16 09:55:49 -0500

    Updates

--- src/lab4/Computer.java
+++ src/lab4/Computer.java
@@ -1,13 +1,9 @@
 package lab4;
 
-import java.util.Arrays;
-
-import lab2.BitTools;
-import lab2.LogicGate;
-
 public class Computer {
 
     private Word mq, ac;
+    private long accumulator;
     private Memory memory;
     private String description;
 
@@ -16,6 +12,7 @@ public class Computer {
         
         mq = new Word();
         ac = new Word();
+        accumulator = 0;
         description = "";
     }
 
@@ -32,53 +29,36 @@ public class Computer {
             break;
         case Opcode.LOADMXNEG:
             // load -mx to ac
-            ac = -mx[address];
-            
-            ac = memory.getWord(address);
             break;
         case Opcode.LOADMXABS:
             // load |mx| to ac
-            ac = mx[address] < 0 ? -mx[address] : mx[address];
             break;
         case Opcode.LOADMXABSNEG:
             // load -|mx| to ac
-            ac = mx[address] < 0 ? mx[address] : -mx[address];
             break;
         case Opcode.ADDMX:
             // add mx to ac
-            ac += mx[address];
             break;
         case Opcode.SUBMX:
             // sub mx from ac
-            ac -= mx[address];
             break;
         case Opcode.ADDMXABS:
             // add |mx| to ac
-            ac += mx[address] < 0 ? -mx[address] : mx[address];
             break;
         case Opcode.SUBMXABS:
             // sub |mx| from ac
-            ac -= mx[address] < 0 ? -mx[address] : mx[address];
             break;
         case Opcode.LOADMQMX:
             // xfer mx to mq
-            mq = mx[address];
             break;
         case Opcode.LOADMQ:
             // xfer mq to ac
-            ac = mq;
             break;
         case Opcode.MULMX:
             // multiply ac by mx, most sig in ac, least sig in mq
-            ac *= mx[address];
-            decimalToBin(ac);
-            ac = binToDec(left);
-            mq = binToDec(right);
             break;
         case Opcode.DIVMX:
             // divide ac by mx, quotient to mq, remainder in ac
-            mq = ac / mx[address];
-            ac %= mx[address];            
             break;
         case Opcode.JUMPMXLEFT:
             // left instruction from mx
@@ -88,39 +68,24 @@ public class Computer {
             break;
         case Opcode.JUMPMXPOSLEFT:
             // ac >=0 left instruction
-            if(ac >= 0){
-                
-            }else{
-                
-            }
             break;
         case Opcode.JUMPMXPOSRIGHT:
             // ac >=0 right instruction
-            if(ac >=0){
-                
-            }else{
-                
-            }
             break;
         case Opcode.STORMX:
             // xfer ac to mx
-            mx[address] = (int) ac;
             break;
         case Opcode.STORMXLEFT:
             // set mx left to ac right
-            storMXLeft(address);            
             break;
         case Opcode.STORMXRIGHT:
             // set mx right to ac right
-            storMXRight(address);
             break;
         case Opcode.LSH:
             // multiply ac by 2
-            ac *= 2;
             break;
         case Opcode.RSH:
             // divide ac by 2 (will be floored)
-            ac /= 2;
             break;
         default:
             setDesc = false;
