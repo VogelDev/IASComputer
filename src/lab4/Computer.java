26573be04e636ca300269c7e0f702aa50faaedea
VogelDev <rvogel@mc3.edu>
2015-02-18 10:49:03 -0500

    Instructions
    Instructions are added in compute method.
    
    Jumps and left/right stores are not done yet.

--- src/lab4/Computer.java
+++ src/lab4/Computer.java
@@ -32,6 +32,7 @@ public class Computer {
             // mar = ibr address
             mar = (int) (ibr & Integer.valueOf("fff", 16));
             // pc++
+            pc++;
         } else {
             mar = pc;
             mbr = memory.getMemory(mar);
@@ -68,54 +69,79 @@ public class Computer {
             break;
         case Opcode.LOADMX:
             // load mx to ac
+            ac = memory.getMemory(address);
             break;
         case Opcode.LOADMXNEG:
             // load -mx to ac
+            ac = -memory.getMemory(address);
             break;
         case Opcode.LOADMXABS:
             // load |mx| to ac
+            ac = Math.abs(memory.getMemory(address));
             break;
         case Opcode.LOADMXABSNEG:
             // load -|mx| to ac
+            ac = -Math.abs(memory.getMemory(address));
             break;
         case Opcode.ADDMX:
             // add mx to ac
+            ac += memory.getMemory(address);
             break;
         case Opcode.SUBMX:
             // sub mx from ac
+            ac -= memory.getMemory(address);
             break;
         case Opcode.ADDMXABS:
             // add |mx| to ac
+            ac += Math.abs(memory.getMemory(address));
             break;
         case Opcode.SUBMXABS:
             // sub |mx| from ac
+            ac -= Math.abs(memory.getMemory(address));
             break;
         case Opcode.LOADMQMX:
             // xfer mx to mq
+            mq = memory.getMemory(address);
             break;
         case Opcode.LOADMQ:
             // xfer mq to ac
+            ac = mq;
             break;
         case Opcode.MULMX:
             // multiply ac by mx, most sig in ac, least sig in mq
             break;
         case Opcode.DIVMX:
             // divide ac by mx, quotient to mq, remainder in ac
+            mq = ac / memory.getMemory(address);
+            ac %= memory.getMemory(address);
             break;
         case Opcode.JUMPMXLEFT:
             // left instruction from mx
+            left = true;
+            pc = mar;
             break;
         case Opcode.JUMPMXRIGHT:
             // right instruction from mx
+            left = false;
+            pc = mar;
             break;
         case Opcode.JUMPMXPOSLEFT:
             // ac >=0 left instruction
+            if (ac >= 0) {
+                left = true;
+                pc = mar;
+            }
             break;
         case Opcode.JUMPMXPOSRIGHT:
             // ac >=0 right instruction
+            if (ac >= 0) {
+                left = false;
+                pc = mar;
+            }
             break;
         case Opcode.STORMX:
             // xfer ac to mx
+            memory.setMemory(address, ac);
             break;
         case Opcode.STORMXLEFT:
             // set mx left to ac right
@@ -125,9 +151,11 @@ public class Computer {
             break;
         case Opcode.LSH:
             // multiply ac by 2
+            ac *= 2;
             break;
         case Opcode.RSH:
             // divide ac by 2 (will be floored)
+            ac /= 2;
             break;
         default:
             setDesc = false;
