ddcd69a07a691318e1285de82ae5bd123fcc610b
VogelDev <rvogel@mc3.edu>
2015-03-04 10:23:16 -0500

    In from text
    Added the input from a text files saved in the src folder.

--- src/lab4/Driver.java
+++ src/lab4/Driver.java
@@ -1,17 +1,52 @@
 package lab4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Driver {
 
     public static void main(String[] args) {
 
-	Memory memory = new Memory(100);
+        String[] input = args;
+        try {
+            //input = readLines(input[0]);
+            input = readLines("src/moyer.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        Memory memory = new Memory(100);
 
-	for(int i =0;i<args.length;i++){
-	    memory.setMemory(i, Long.parseLong(args[i], 16));
-	}
-	
-	Computer computer = new Computer(memory);
-	computer.run();
-	System.out.println(memory);
+        for (int i = 0; i < input.length; i++) {
+            memory.setMemory(i, Long.parseLong(input[i], 16));
+        }
+        System.out.println(memory);
+
+        Computer computer = new Computer(memory);
+        computer.run();
+        System.out.println(memory);
+    }
+
+    public static String[] readLines(String filename) throws IOException {
+        FileReader reader = new FileReader(filename);
+
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        List<String> lines = new ArrayList<String>();
+        String line = null;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            if(line.contains("#"))
+                lines.add(line.substring(0, line.indexOf("#")));
+            else
+                lines.add(line);
+        }
+
+        bufferedReader.close();
+        
+        return lines.toArray(new String[lines.size()]);
     }
 }
